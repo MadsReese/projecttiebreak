eb919fe6c684b9951e110533d22976f2786e6845
MadsReese <mads.reese@gmail.com>
2013-04-02 10:43:48 +0200

    Completes the functions in DAL for members

--- ProjectTieBreak/src/DAL/MemberAccess.java
+++ ProjectTieBreak/src/DAL/MemberAccess.java
@@ -56,7 +56,7 @@ public class MemberAccess
     {
         try (Connection con = connector.getConnection())
         {
-            String sql = "SELECT * FROM Team WHERE Id = ?";
+            String sql = "SELECT * FROM Member WHERE Id = ?";
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setInt(1, id);
 
@@ -66,6 +66,60 @@ public class MemberAccess
                 return getOneMember(rs);
             }
             return null;
+        }
+    }
+    
+    public Member getByDTU(int DTUNo) throws SQLServerException, SQLException
+    {
+        try (Connection con = connector.getConnection())
+        {
+            String sql = "SELECT * FROM Member WHERE DTUNo = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, DTUNo);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next())
+            {
+                return getOneMember(rs);
+            }
+            return null;
+        }
+    }
+    
+    public void update(Member m) throws SQLServerException, SQLException
+    {
+        try (Connection con = connector.getConnection())
+        {
+            String sql = "UPDATE Member SET firstName = ?, lastName = ?, address = ?, phoneNo = ?, email = ? WHERE Id = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, m.getFirstName());
+            ps.setString(2, m.getLastName());
+            ps.setString(3, m.getAddress());
+            ps.setInt(4, m.getPhoneNo());
+            ps.setString(5, m.getEmail());
+            ps.setInt(6, m.getMemberNo());
+            
+            int affectedRows = ps.executeUpdate();
+            if (affectedRows < 1)
+            {
+                throw new SQLException("Unable to update member");
+            }
+        }
+    }
+    
+    public void delete(int id) throws SQLServerException, SQLException
+    {
+        try (Connection con = connector.getConnection())
+        {
+            String sql = "DELETE FROM Member WHERE Id = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            
+            int affectedRows = ps.executeUpdate();
+            if (affectedRows < 1)
+            {
+                throw new SQLException("Unable to delete member");
+            }
         }
     }
     private Member getOneMember(ResultSet rs) throws SQLException
@@ -80,6 +134,14 @@ public class MemberAccess
         String memberType = rs.getString(8);
         int DTULicence = rs.getInt(9);
         
-        
+        return Member.fromDataBase(memberNo, 
+                lastName, 
+                firstName, 
+                address, 
+                birthYear, 
+                phoneNo, 
+                email, 
+                memberType, 
+                DTULicence);
     }
 }
