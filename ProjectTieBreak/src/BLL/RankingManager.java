647c1970906271d6f628f9b2cb6f829ea3620255
boinq <boinq@MintyFresh>
2013-04-11 09:56:17 +0200

    Ranking code delete

--- ProjectTieBreak/src/BLL/RankingManager.java
+++ ProjectTieBreak/src/BLL/RankingManager.java
@@ -10,32 +10,5 @@ import java.util.List;
  */
 public class RankingManager 
 {
-    private static final int WIN_POINTS = 3, LOSS_POINTS = 0, DRAW_POINTS = 1;
     
-    public static Member[] rankMembers(List<Member> members, List<Match> matches)
-    {
-        // Initialize an array for holding the ranked members.
-        Member[] ranked = new Member[members.size()];
-        // Now put the members in, and shift them around based on the ranking rules;
-        boolean done = false;
-        int index = 0;
-        while (!done)
-        {
-            for (Member t : members)
-            {
-                if (ranked[index] == null)
-                {
-                    ranked[index] = t;
-                }
-                else
-                {
-                    ranked[index] = compareMembers(ranked[index], t, matches);
-                }
-            }
-            members.remove(ranked[index++]);
-            done = members.isEmpty();
-        }
-        return ranked;
-    }
-
 }
